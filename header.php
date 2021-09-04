fbb31d93a3bb59fca28e54aff90f567ea3d92ef2
Max1O <fcb10max@gmail.com>
2021-09-04 22:39:17 +0500

    stable beta

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Longview</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+
+<header>
+
+    <div class="header">
+        <div class="header-wrap">
+            <img class="logo" src="<?php echo get_template_directory_uri() . '/images/logo.jpg' ?>" alt="">
+            <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/menu.png' ?>"></img></a>
+        </div>
+    </div>
+
+</header>
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php get_header(); ?>
+
+<div class="about-images">
+
+    <div class="notebook">
+        <img width="945px" height="410px" src="<?php echo get_template_directory_uri() . '/images/notebook.jpg'?>" alt="img">
+    </div>
+    <div class="about-text-layer">
+        <p class="about-text">About LongView</p>
+        <img width="1245px" height="460px" src="<?php echo get_template_directory_uri() . '/images/index.png'?>" alt="img">
+    </div>
+
+</div>
+
+<div class="content-intro">
+    <div class="intro-text">
+        <div class="one-line-text">
+            <p>New solutions</p>
+            <div class="one-line-text-line"></div>
+        </div>
+        <div class="larger-text">
+            <p class="larger-text-title">To old problems</p>
+            <p class="larger-text-content">Every real estate agency gives you the same service, the same products and treats property management as the poor relation to their lucrative sales business. We are different. We see the day-to-day service of property management as our crown jewel.<br><br>More importantly, we see a whole range of needs that the broader, traditional real estate industry has ignored for a long time. We are setting about to change</p>
+        </div>
+    </div>
+    <div class='content-image1'>
+        <img width="1170px" height="630px" src="<?php echo get_template_directory_uri() . '/images/discussion2.jpg'?>" alt="">
+    </div>
+    <div class="intro-text">
+        <div class="one-line-text">
+            <p>The LongView vision </p>
+            <div class="one-line-text-line two"></div>
+        </div>
+        <div class="larger-text">
+            <p class="larger-text-title">Lorem ipsum dolor sit amet consectetur</p>
+            <p class="larger-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.<br>More importantly, we see a whole range of needs that the broader, traditional real estate industry has ignored for a long time. We are setting about to change this and offer simple, fair and unique solutions for those needs. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.</p>
+        </div>
+    </div>
+
+</div>
+
+<?php get_footer(); ?>
