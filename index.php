c7d9a21a4c3aeb1caac743b8ea33cafbfc44978f
Max1O <fcb10max@gmail.com>
2021-09-05 15:25:51 +0500

    stable beta2

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 </div>
 
 <div class="content-intro">
+    <div class='bg-div'></div>
     <div class="intro-text">
         <div class="one-line-text">
             <p>New solutions</p>
@@ -33,10 +34,101 @@
         </div>
         <div class="larger-text">
             <p class="larger-text-title">Lorem ipsum dolor sit amet consectetur</p>
-            <p class="larger-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.<br>More importantly, we see a whole range of needs that the broader, traditional real estate industry has ignored for a long time. We are setting about to change this and offer simple, fair and unique solutions for those needs. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.</p>
+            <p class="larger-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.<br><br>More importantly, we see a whole range of needs that the broader, traditional real estate industry has ignored for a long time. We are setting about to change this and offer simple, fair and unique solutions for those needs. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.</p>
         </div>
     </div>
-
+    <div class='nav-bars'>
+        <div class='nav-bars-wrap'>
+            <div class='nav-bar-item'>
+                <div class='nav-bar-item-wrap'>
+                    <p class='nav-title'>Sub-title<br>…</p>
+                    <img width='63px' height='50px' src="<?php echo get_template_directory_uri() . '/images/house.png'?>" alt="img">
+                    <p class='nav-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                </div>
+            </div>
+            <div class='nav-bar-item'>
+                <div class='nav-bar-item-wrap'>
+                    <p class='nav-title'>Sub-title<br>…</p>
+                    <img width='39px' height='50px' src="<?php echo get_template_directory_uri() . '/images/contract.png'?>" alt="img">
+                    <p class='nav-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+            <div class='nav-bar-item'>
+                <div class='nav-bar-item-wrap'>
+                    <p class='nav-title'>Sub-title<br>…</p>
+                    <img width='64px' height='50px' src="<?php echo get_template_directory_uri() . '/images/insurance.png'?>" alt="img">
+                    <p class='nav-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+                </div>
+            </div>
+            <div class='nav-bar-item'>
+                <div class='nav-bar-item-wrap'>
+                    <p class='nav-title'>Sub-title<br>…</p>
+                    <img width='55px' height='50px' src="<?php echo get_template_directory_uri() . '/images/open_house.png'?>" alt="img">
+                    <p class='nav-text'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<div class="rent-content-wrap">
+    <div class="intro-text">
+            <div class="one-line-text">
+                <p>The Guaranteed Rent company</p>
+                <div class="one-line-text-line three"></div>
+            </div>
+            <div class="larger-text">
+                <p class="larger-text-title">The Guaranteed Rent company</p>
+                <p class="larger-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.  Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+    </div>
+</div>
+
+<div class='marketing-content'>
+    <div class='marketing-content-rect1'></div>
+    <div class='marketing-content-rect2'></div>
+    <div class='marketing-content-wrap'>
+        <div class="marketing-content-item">
+            <div class='marketing-content-item-text'>
+                <p class='marketing-content-item-title'>Buy with<br>Guaranteed Rent</p>
+                <p class='marketing-content-item-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.  Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class='marketing-content-item-img'>
+                <img width='570px' height="413px" src="<?php echo get_template_directory_uri() . '/images/relaxing_man.jpg'?>" alt="">
+            </div>
+        </div>
+        <div class="marketing-content-item">
+            <div class='marketing-content-item-img'>
+                <img width='570px' height="413px" src="<?php echo get_template_directory_uri() . '/images/working_woman.jpg'?>" alt="">
+            </div>
+            <div class='marketing-content-item-text second'>
+                <p class='marketing-content-item-title'>Manage with<br>Guaranteed Rent</p>
+                <p class='marketing-content-item-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.  Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.</p>
+            </div>
+        </div>
+        <div class="marketing-content-item">
+            <div class='marketing-content-item-text'>
+                <p class='marketing-content-item-title'>Sell with<br>Guaranteed Rent</p>
+                <p class='marketing-content-item-description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+            </div>
+            <div class='marketing-content-item-img'>
+                <img width='570px' height="413px" src="<?php echo get_template_directory_uri() . '/images/discussion.jpg'?>" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="testimonials-content-wrap">
+    <div class="intro-text">
+            <div class="one-line-text">
+                <p>Testimonials</p>
+                <div class="one-line-text-line testimonial-line"></div>
+            </div>
+            <div class="larger-text">
+                <p class="larger-text-title">What people say about us</p>
+            </div>
+    </div>
+</div>
+
 
 <?php get_footer(); ?>
